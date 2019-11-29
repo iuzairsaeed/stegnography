0176cd406a21c1c3673977993a3276c058a53757
Uzair Saeed <uzair.saeed27@gmail.com>
2019-11-29 15:26:48 +0500

    add files

---
+++ readme.md
@@ -0,0 +1,27 @@
+<p align="center"><img src="https://laravel.com/assets/img/components/logo-laravel.svg"></p>
+
+## About
+
+Steganography coder/decoder and analyzer
+
+## System Requirements
+
+The Laravel framework has a few system requirements. However, if you are not using Homestead, you will need to make sure your server meets the following requirements:
+
+PHP >= 5.6.4 OpenSSL PHP Extension PDO PHP Extension Mbstring PHP Extension Tokenizer PHP Extension XML PHP Extension
+
+## Installation
+
+Please check the system requirements before installing project
+
+You may install by cloning from github
+Github: git clone https://github.com/Lintume/Leader-crawler
+Enter your database details into app/config/database.php.
+Run the command composer install
+For creating tables in data base run the command
+`php artisan migrate`
+Finally, setup an Apache VirtualHost to point to the "public" folder.
+For development, you can simply run
+`php artisan serve`
+
+
---
+++ routes/web.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/vkauthorize', function () {
+    return '<a href="https://oauth.vk.com/authorize?client_id=4506839&scope=status,friends&redirect_uri=http://api.vk.com/blank.html&display=page&response_type=token">Push the button</a>';
+});
+
+Route::get('/vklintu', function () {
+    return '<a href="https://oauth.vk.com/authorize?client_id=5929085&scope=status,friends&redirect_uri=http://api.vk.com/blank.html&display=page&response_type=token">Push the button</a>';
+});
+
+Route::get('/leaders', ['as' => 'leaders', 'uses' => 'MemberController@show']);
+
+Route::get('/steganography', ['as' => 'steganography', 'uses' => 'AnalyzeController@index']);
+Route::post('/analyze', ['as' => 'analyze', 'uses' => 'AnalyzeController@analyze']);
+
+Route::get('/lsb_view', ['as' => 'lsb', 'uses' => 'LSBController@encodeLSB']);
+Route::post('/lsb_encode', ['as' => 'lsb_encode', 'uses' => 'LSBController@LSBAnalyzeEncode']);
+Route::post('/lsb_decode', ['as' => 'lsb_decode', 'uses' => 'LSBController@LSBAnalyzeDecode']);
+
+Route::get('/lsb_crypt_view', ['as' => 'lsbCrypt', 'uses' => 'LSBCryptController@encodeLSBCrypt']);
+Route::post('/lsb_encode_crypt', ['as' => 'lsb_encode_crypt', 'uses' => 'LSBCryptController@LSBEncodeCrypt']);
+Route::post('/lsb_decode_crypt', ['as' => 'lsb_decode_crypt', 'uses' => 'LSBCryptController@LSBDecodeCrypt']);
+
+Route::get('/lsb_offset_view', ['as' => 'lsbOffset', 'uses' => 'LSBOffsetController@showOffsetLSB']);
+Route::post('/lsb_offset_encode', ['as' => 'lsb_encode_offset', 'uses' => 'LSBOffsetController@LSBOffsetEncode']);
+Route::post('/lsb_offset_decode', ['as' => 'lsb_decode_offset', 'uses' => 'LSBOffsetController@LSBOffsetDecode']);
+
+Route::get('/lsb_2bits_view', ['as' => 'lsb2bits', 'uses' => 'LSB2LastBitsBlueController@show']);
+Route::post('/lsb_2bits_encode', ['as' => 'lsb_encode_2bits', 'uses' => 'LSB2LastBitsBlueController@encode']);
+Route::post('/lsb_2bits_decode', ['as' => 'lsb_decode_2bits', 'uses' => 'LSB2LastBitsBlueController@decode']);
+
+Route::get('/lsb_3channels_view', ['as' => 'lsb3channels', 'uses' => 'LSB3channelsController@show']);
+Route::post('/lsb_3channels_encode', ['as' => 'lsb_encode3channels', 'uses' => 'LSB3channelsController@encode']);
+Route::post('/lsb_3channels_decode', ['as' => 'lsb_decode3channels', 'uses' => 'LSB3channelsController@decode']);
+
+Route::post('/rand', ['as' => 'rand', 'uses' => 'randomController@rand']);
+
+    Route::get('vk', function () {
+        $client_id = '5922811';
+        $scope = 'status';
+        $group_ids = '48715015';
+        echo "<a href=\"https://oauth.vk.com/authorize?client_id=$client_id&display=page&redirect_uri=http://mytattoo.sexy/oauth/vk/callback&scope=$scope&group_ids=$group_ids&response_type=code&v=5.62\">Push the button</a>";
+    });
+    Route::get('vk/callback', function (\Illuminate\Http\Request $request) {
+        $client_id = '5922811';
+        $client_secret = 'MXDFuLCCA5Or6LTWSP8K';
+        if ($request->has('code')) {
+            $code = $request->get('code');
+            echo "<a href=\"https://oauth.vk.com/access_token?client_id=$client_id&client_secret=$client_secret&redirect_uri=http://mytattoo.sexy/oauth/vk/callback&code=$code\">Get the code</a>";
+        }
+        if ($request->has('access_token')) {
+            dd($request->get('access_token'));
+        }
+    });
+
+
+Route::get('/lsb_index','Api\StegnographyController@index');
+Route::post('/alsb_encode_crypt', ['as' => 'alsb_encode_crypt', 'uses' => 'Api\StegnographyController@LSBEncodeCrypt']);
+Route::post('/alsb_decode_crypt', ['as' => 'alsb_decode_crypt', 'uses' => 'Api\StegnographyController@LSBDecodeCrypt']);
